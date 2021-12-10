98888d169c413c209ef9c42f72da1dabcac09362
Bonu93 <luca.morucci.kb@gmail.com>
2021-12-10 17:07:02 +0100

    add calendar

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,100 +1,161 @@
 <template>
-  <main>
-      <section class="h500">
-          <div class="site-lg-container bg-light specs d-flex flex-column justify-content-center align-items-center text-center p-5">
-              <div class="container d-flex flex-column justify-content-center align-items-center">
+    <main>
+        <section class="h500">
+            <div
+                class="site-lg-container bg-light specs d-flex flex-column justify-content-center align-items-center text-center p-5"
+            >
+                <div
+                    class="container d-flex flex-column justify-content-center align-items-center"
+                >
                     <h2 class="pointed">Our specialities</h2>
                     <span class="subtitle">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, praesentium?
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Sequi, praesentium?
                     </span>
-                    <hr class="orange-rule">
+                    <hr class="orange-rule" />
                     <div class="row justify-content-center g-0 w-75">
                         <div class="col-3">
-                            <div class="spec-card d-flex flex-column justify-content-center">
-                                <img src="../assets/svg/svg-0.svg" alt="icon">
+                            <div
+                                class="spec-card d-flex flex-column justify-content-center"
+                            >
+                                <img src="../assets/svg/svg-0.svg" alt="icon" />
                                 <h4>Knowing</h4>
                                 <span class="subtitle">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit.
                                 </span>
                             </div>
                         </div>
                         <div class="col-3">
-                            <div class="spec-card d-flex flex-column justify-content-center">
-                                <img src="@/assets/svg/svg-0.svg" alt="icon">
+                            <div
+                                class="spec-card d-flex flex-column justify-content-center"
+                            >
+                                <img src="@/assets/svg/svg-0.svg" alt="icon" />
                                 <h4>Knowing</h4>
                                 <span class="subtitle">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit.
                                 </span>
                             </div>
                         </div>
                         <div class="col-3">
-                            <div class="spec-card d-flex flex-column justify-content-center">
-                                <img src="@/assets/svg/svg-0.svg" alt="icon">
+                            <div
+                                class="spec-card d-flex flex-column justify-content-center"
+                            >
+                                <img src="@/assets/svg/svg-0.svg" alt="icon" />
                                 <h4>Knowing</h4>
                                 <span class="subtitle">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit.
                                 </span>
                             </div>
                         </div>
                         <div class="col-3">
-                            <div class="spec-card d-flex flex-column justify-content-center">
-                                <img src="@/assets/svg/svg-0.svg" alt="icon">
+                            <div
+                                class="spec-card d-flex flex-column justify-content-center"
+                            >
+                                <img src="@/assets/svg/svg-0.svg" alt="icon" />
                                 <h4>Knowing</h4>
                                 <span class="subtitle">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit.
                                 </span>
                             </div>
                         </div>
                     </div>
-              </div>
-          </div>
-      </section>
+                </div>
+            </div>
+        </section>
 
-      <section class="h500 member">
-          <div class="container d-flex justify-content-center">
-              <div class="row w-75 g-0 justify-content-center">
-                  <div class="col-6">
-                      <div class="member-image">
-                          <img src="@/assets/img/h1-img-01.jpg" alt="founder">
-                      </div>
-                  </div>
-                  <div class="col-6 d-flex align-items-center">
-                      <div class="member-description bg-light p-5">
-                          <h3>Jason BickFord</h3>
-                          <span class="subtitle">Founder and Executive Director</span>
-                          <hr class="orange-rule">
-                          <p class="subtitle">
-                              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore voluptates, animi doloribus quam laboriosam dolorem quasi.
-                          </p>
-                          <div class="social-icons">
-                              <i class="fab fa-linkedin-in"></i>
-                              <i class="fab fa-facebook"></i>
-                              <i class="fab fa-twitter"></i>
+        <section class="h500 member">
+            <div class="container d-flex justify-content-center">
+                <div class="row w-75 g-0 justify-content-center">
+                    <div class="col-6">
+                        <div class="member-image">
+                            <img
+                                src="@/assets/img/h1-img-01.jpg"
+                                alt="founder"
+                            />
+                            <div class="arrows">
+                                <a href="#">&#10229;</a>
+                                <a href="#">&#10230;</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6 d-flex align-items-center">
+                        <div class="member-description bg-light p-5">
+                            <h3>Jason BickFord</h3>
+                            <span class="subtitle"
+                                >Founder and Executive Director</span
+                            >
+                            <hr class="orange-rule" />
+                            <p class="subtitle">
+                                Lorem ipsum dolor sit, amet consectetur
+                                adipisicing elit. Labore voluptates, animi
+                                doloribus quam laboriosam dolorem quasi.
+                            </p>
+                            <div class="social-icons">
+                                <i class="fab fa-linkedin-in"></i>
+                                <i class="fab fa-facebook"></i>
+                                <i class="fab fa-twitter"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
 
-                          </div>
-                      </div>
+        <section class="h700 coaching d-flex justify-content-center align-items-center">
+            <div class="container d-flex justify-content-center">
+                <div class="row w-75">
+                    <div class="col-5">
+                        <div class="calendar d-flex flex-column bg-light">
+                            <div class="calendar-title p-4">
+                                <h3 class="m-0">Upcoming Events</h3>
+                            </div>
+                            <div class="calendar-event d-flex p-4">
+                                <div class="date text-center">
+                                    <span class="day">07</span> Jan, 2022
+                                </div>
+                                <div class="event-details d-flex flex-column mx-3">
+                                    <h4>Melbourne Coaching</h4>
+                                    <div class="time">
+                                        <span class="subtitle">
+                                            9:00 am - 5:00pm, Jan 7, 2022
+                                        </span>
+                                    </div>
+                                    <div class="location">
+                                        <span class="subtitle">
+                                            Cambridge, MA 02138, USA
+                                        </span>
+                                    </div>
+                                    <a href="#" class="read-more">Read More</a>
+                                </div>
+                            </div>
 
-                  </div>
-              </div>
-          </div>
-      </section>
-  </main>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    </main>
 </template>
 
 <script>
 export default {
-    name: 'Main',
-}
+    name: "Main",
+};
 </script>
 
 <style lang="scss" scoped>
-@import '@/styles/globals.scss';
+@import "@/styles/globals.scss";
 
 main {
     background: $bg-grey;
 }
 
-// SPECS SECTION 
+// SPECS SECTION
 .specs {
     position: relative;
     top: -60px;
@@ -109,7 +170,7 @@ main {
     justify-content: center;
     align-items: center;
     // padding: 30px;
-    h4{
+    h4 {
         font-size: 1rem;
     }
     img {
@@ -117,12 +178,23 @@ main {
         object-fit: contain;
         margin-bottom: 1rem;
     }
-    
 }
 
-// MEMBERS SECTION 
+// MEMBERS SECTION
 
 .member-image {
+    position: relative;
+    .arrows {
+        position: absolute;
+        bottom: 0;
+        background: $orange;
+        padding: 0.8rem;
+        a {
+            color: white;
+            text-decoration: none;
+            margin: 0.5rem;
+        }
+    }
     img {
         width: 100%;
     }
@@ -135,7 +207,7 @@ main {
     .social-icons {
         color: $orange;
         i {
-            margin-right: .5rem;
+            margin-right: 0.5rem;
             font-size: 1rem;
             cursor: pointer;
             &:hover {
@@ -145,6 +217,38 @@ main {
     }
 }
 
+// COACHING SECTION
 
+.coaching {
+    background-image: url('../assets/img/h1-img-09.jpg');
+    background-position-y: 30%;
+    background-size: cover;
+    font-size: 12px;
+    .calendar-title {
+        box-shadow: rgba(33, 35, 38, 0.1) 0px 10px 10px -10px;
+    }
+    h4 {
+        font-size: 1.2rem;
+    }
+    .date {
+        background: $orange;
+        color: white;
+        height: 50%;
+        padding: .5rem;
+        .day {
+            display: block;
+            font-size: 1rem;
+        }
+    }
+}
+
+.calendar-event{
+    cursor: pointer;
+    &:hover {
+        h4 {
+            color: $orange;
+        }
+    }
+}
 
 </style>
